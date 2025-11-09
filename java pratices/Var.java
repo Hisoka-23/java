36e23c2a7eccf114d2be6c7b0ae1d9bac6b103ab
Prakash <prakash>
2025-11-09 19:46:49 +0530

    Arrays & string

---
+++ java pratices/Var.java
@@ -0,0 +1,15 @@
+class Var{
+	public static void main(String... args){
+		int a = 23;
+		System.out.println(a);
+
+		float b = 23.23f;
+		System.out.println(b);
+		
+		char c = 'p';
+		System.out.println(c);
+
+		boolean  d = true;
+		System.out.println(d);
+	}
+}
